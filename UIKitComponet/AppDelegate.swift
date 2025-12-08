b3e3c4d17609995de34456eb9b046b1443c06bb6
Omkar Shisode <76614558+omkarshisode@users.noreply.github.com>
2025-12-08 23:34:34 +0530

    UI window removed from the appDelegate

--- UIKitComponet/AppDelegate.swift
+++ UIKitComponet/AppDelegate.swift
@@ -9,19 +9,8 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        window = UIWindow(frame: UIScreen.main.bounds)
-//        let viewController = ViewController()
-//        let rootViewController = UINavigationController(rootViewController: viewController)
-//        window?.rootViewController = rootViewController
-        window?.makeKeyAndVisible()
-        
         return true
     }
-    
 }
 
